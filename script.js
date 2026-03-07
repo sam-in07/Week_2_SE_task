bd7c3add241a41c240c3edef51f9dca15c7d5b80
Tanzim313 <tanzimhasanrizbi@gmail.com>
2026-03-08 00:58:32 +0600

    Resolved merge conflict

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById("greet").innerText = "Hello from Team!!!";
 document.getElementById("timeMsg").innerText = "Hello from Team Updated!";
 
+console.log("Team Project - Dev1 & Dev2");
